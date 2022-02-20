0a40494e24eb74d3af7afb59cee76163f03f0e5c
xtareq <xtrinsic96@gmail.com>
2022-02-20 21:50:56 +0600

    added todo

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,85 @@
 import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import styled, { createGlobalStyle, ThemeProvider } from 'styled-components'
+import { FiPlus, FiSun } from 'react-icons/fi'
+
+
+
+const ThemeButton = styled.button`
+      position:fixed;
+      right:20px;
+      border-radius:24px;
+      color: ${props=>props.theme.mode == "dark" ? props.theme.light:props.theme.dark };
+      border:${props=>props.theme[props.theme.mode]};
+      background-color:${props=>props.theme[props.theme.mode]};
+
+
+
+`
+
+const AddTodo = styled.div`
+  padding:10px;
+  display:flex;
+  gap:4px;
+  input{
+    width:100%;
+    padding:8px 4px;
+    &:hover,&:focus-within{
+      border-color:1px solid ${props=>props.theme.primary};
+    }
+  }
+
+  button{
+    font-size:16px;
+    font-weight:bold;
+    background-color:${props=>props.theme.primary};
+    border:1px solid ${props=>props.theme.primary};
+    color:white;
+
+    &:hover,&:active{
+      background-color:#3f016c;
+    }
+
+  }
+`
+
+const GlobalSytle = createGlobalStyle`
+
+  body{
+    margin:0;
+    padding:0;
+    color: ${props=>props.theme.mode == "dark" ? props.theme.light:props.theme.dark };
+    background-color:${props=>props.theme[props.theme.mode]}
+  }
+
+`
 
 function App() {
   const [count, setCount] = useState(0)
+  const [darkMode, setDarkMode] = useState(false)
+
+  const theme = {
+    mode:'light',
+    light:'#ffffff',
+    dark: '#242323',
+    primary:'indigo'
+  }
+
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
+   <ThemeProvider theme={{...theme,mode:darkMode?'dark':'light'}}>
+     <GlobalSytle/>
+     <ThemeButton onClick={()=>setDarkMode(!darkMode)}>
+       <FiSun/>
+     </ThemeButton>
+
+     <div>
+       <h3>Todo List</h3>
+       <AddTodo>
+         <input placeholder='Add Todo' />
+         <button><FiPlus/></button>
+       </AddTodo>
+     </div>
+   </ThemeProvider>
   )
 }
 
---
+++ src/components/Todo.js
@@ -0,0 +1,16 @@
+import styled from "styled-components"
+
+
+
+
+const StyledTodo = styled.div`
+
+
+`
+
+const Todo = () => {
+
+}
+
+
+export default Todo
